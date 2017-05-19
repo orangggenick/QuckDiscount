5e16977addb0a9526f77bfc959b0841f339a841a
isakov <orangggenick@mail.ru>
2017-05-19 19:22:52 +0600

    Settings
    Models are registred in Admin panel. Migrations are made and applied. Superuser is created. Static and media root are сonfigured

--- QuickDiscount/Discount/admin.py
+++ QuickDiscount/Discount/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from Discount.models import Customer, Seller, Shop, Stock, Subscription
+
+admin.site.register(Customer)
+admin.site.register(Seller)
+admin.site.register(Shop)
+admin.site.register(Stock)
+admin.site.register(Subscription)
