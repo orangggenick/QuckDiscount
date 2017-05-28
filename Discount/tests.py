ba89888dee3d1353ef12b52a883c6b3542b32fbb
isakov <orangggenick@mail.ru>
2017-05-28 22:00:18 +0600

    FavoritesTest added

--- Discount/tests.py
+++ Discount/tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from Discount.models import Shop, Stock, Subscription
+from Discount.models import Shop, Stock, Subscription, Favorites
 
 
 class CommonTest(TestCase):
@@ -43,4 +43,34 @@ class SubscriptionTest(TestCase):
         subscription = Subscription.objects.get(user_id = user.id)
         stocks = Stock.objects.filter(shop_id=subscription.shop_id)
         self.assertEqual(stocks[0].exposition, "Test exposition3")
+        self.assertEqual(stocks[1].exposition, "Test exposition4")
+
+
+class FavoritesTest(TestCase):
+    def setUp(self):
+        User.objects.create(username="Test", password="qwer1234")
+        user = User.objects.get(username="Test")
+        Shop.objects.create(name="Test Shop", category="Одежда", seller_id=user.id)
+        shop = Shop.objects.get(name="Test Shop")
+        Stock.objects.create(exposition="Test exposition", terms="2016-01-02", description="Test description", date="2016-01-01", is_active=True, shop_id=shop.id)
+        Stock.objects.create(exposition="Test exposition2", terms="2016-01-02", description="Test description2",date="2016-01-01", is_active=True, shop_id=shop.id)
+        stock1 = Stock.objects.get(exposition="Test exposition")
+        stock2 = Stock.objects.get(exposition="Test exposition2")
+        Shop.objects.create(name="Test Shop2", category="Одежда", seller_id=user.id)
+        shop2 = Shop.objects.get(name="Test Shop2")
+        Stock.objects.create(exposition="Test exposition3", terms="2016-01-02", description="Test description3", date="2016-01-01", is_active=True, shop_id=shop2.id)
+        Stock.objects.create(exposition="Test exposition4", terms="2016-01-02", description="Test description4", date="2016-01-01", is_active=True, shop_id=shop2.id)
+        stock3 = Stock.objects.get(exposition="Test exposition3")
+        stock4 = Stock.objects.get(exposition="Test exposition4")
+        Favorites.objects.create(user_id = user.id, stock_id = stock2.id)
+        Favorites.objects.create(user_id=user.id, stock_id=stock4.id)
+
+
+    def test(self):
+        user = User.objects.get(username="Test")
+        favorites = Favorites.objects.filter(user_id = user.id)
+        stocks = []
+        for favorite in favorites:
+            stocks.append(Stock.objects.get(id=favorite.stock_id))
+        self.assertEqual(stocks[0].exposition, "Test exposition2")
         self.assertEqual(stocks[1].exposition, "Test exposition4")
---
+++ build/html/_sources/index.rst.txt
@@ -0,0 +1,20 @@
+.. QuickDiscount documentation master file, created by
+   sphinx-quickstart on Sat May 27 22:22:08 2017.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to QuickDiscount's documentation!
+=========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
